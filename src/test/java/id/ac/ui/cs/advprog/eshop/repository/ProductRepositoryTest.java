a40369d20d12d61d515f7e65233ea9e68aca1756
msatrioh <90437153+msatrioh@users.noreply.github.com>
2026-02-10 20:15:34 +0700

    ProductRepositoryTest.java commit

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,69 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductRepositoryTest {
+
+    @InjectMocks
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+        product.setProductId("id1");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+
+        productRepository.create(product);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product savedProduct = productIterator.next();
+        assertEquals(savedProduct.getProductId(), product.getProductId());
+        assertEquals(savedProduct.getProductName(), product.getProductName());
+        assertEquals(savedProduct.getProductQuantity(), product.getProductQuantity());
+    }
+
+    @Test
+    void testFindAllIfEmpty() {
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertFalse(productIterator.hasNext());
+    }
+
+    @Test
+    void testFindAllIfMoreThanOneProduct() {
+        Product product1 = new Product();
+        product1.setProductId("id1");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("id2");
+        product2.setProductName("Sampo Cap Usep");
+        product2.setProductQuantity(50);
+        productRepository.create(product2);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product savedProduct = productIterator.next();
+        assertEquals(savedProduct.getProductId(), product1.getProductId());
+        savedProduct =  productIterator.next();
+        assertEquals(savedProduct.getProductId(), product2.getProductId());
+        assertFalse(productIterator.hasNext());
+    }
+}
