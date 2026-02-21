bb30b0bf6537502507c264af5a6d55f9e09b4014
msatrioh <90437153+msatrioh@users.noreply.github.com>
2026-02-21 11:27:52 +0700

    made EditProduct.html, added edit button to ProductList.html, fixed edit get and post mapping in ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -35,4 +35,16 @@ public class ProductController {
         model.addAttribute("products", allProduct);
         return "ProductList";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editProductPage(@PathVariable String id, Model model) {
+        Product product = service.findById(id);
+        model.addAttribute("product", product);
+        return "EditProduct";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editProductPost(@ModelAttribute Product product, Model model) {
+        return "redirect:list";
+    }
 }
